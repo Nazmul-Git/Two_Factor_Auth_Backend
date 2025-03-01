f9b3aa08c025c42bec79da94faa1c81f3c946bbc
Nazmul-Git <nazmul.hasan16021998@gmail.com>
2025-03-02 03:04:23 +0600

    Implement

--- routes/authRouters.js
+++ routes/authRouters.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { register, login, verify2FA } = require('../middleware/auth');
-
 const router = express.Router();
 
 // routes
-
 router.post('/register', register);
 router.post('/login', login);
 router.post('/verify', verify2FA);
+
+router.get('/', (req, res) => {
+  res.send('Welcome to the API');
+});
 
 module.exports = router;
